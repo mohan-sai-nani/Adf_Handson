0417ecc8b940b6b113966a3d13e6c62036497f71
Mohan Sai Regulagaddi <58841715+mohan-sai-nani@users.noreply.github.com>
2021-09-12 16:53:51 +0530

    Delete dbconfig.py

--- Python/Day-6/dbconfig.py
+++
@@ -1,7 +0,0 @@
-"""User Credentials"""
-mysql = {'user': 'mohansai',
-         'password': 'NaNi@sql#143',
-         'host': '127.0.0.1',
-         'database': 'TRAINING'
-         }
-USE_ANONYMOUS = True
