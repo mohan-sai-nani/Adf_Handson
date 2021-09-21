7069e5909101004c580e923cca671df6bb9ec0ce
r.mohansai <mohansai473@gmail.com>
2021-09-21 15:36:23 +0530

    Java Initialization

---
+++ Java/src/Basic_Calculation.java
@@ -0,0 +1,9 @@
+public class Basic_Calculation {
+    public static int add(int a, int b){
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println(add(a,b));
+    }
+}
